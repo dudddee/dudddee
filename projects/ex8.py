0fdcef90755da10c777089874085e5f5a62fa9f3
dudddee <116696782+dudddee@users.noreply.github.com>
2022-10-26 22:53:57 -0300

    Create ex8.py

---
+++ projects/ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "eu sei que vou te amar",
+    "por toda a minha vida eu vou te amar",
+    "em cada despedida eu vou te amar",
+    "desesperadamente eu sei que vou te amar"
+))
