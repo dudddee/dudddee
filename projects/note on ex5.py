acbaecca4f1131df172120070fe364216b2fbe51
dudddee <116696782+dudddee@users.noreply.github.com>
2022-10-28 22:11:01 -0300

    Create note on ex5.py

---
+++ projects/note on ex5.py
@@ -0,0 +1,7 @@
+so, hear me out
+
+this stuff of getting variables into strings is stupdly cool
+
+i'll have to remember the f inside the print to get the variables inside the string
+
+ALSO, i figured out how to round up numbers by putting x:.yf, where x is the variable and y is the number of significative decimals
