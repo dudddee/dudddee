f44f157f56230fdb1c854c08b83d4214e50bc2de
dudddee <116696782+dudddee@users.noreply.github.com>
2022-10-29 11:39:23 -0300

    Create ex15.py

---
+++ projects/ex15.py
@@ -0,0 +1,21 @@
+#imports argv from sys module
+from sys import argv
+
+#assigns two variables to argv, script (the name of the current file) and filename (the name of the file we want to read)
+script, filename = argv
+
+#performs the pydoc function, open, on the file we determined and stores the open file in a variable called txt
+txt = open(filename)
+
+#prints out the name of the file we gave earlier
+print(f"here's yout file {filename}:")
+#prints out the contents of txt by performing a read function without parameters
+print(txt.read())
+
+#does the same but with input instead of argv
+print("type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
