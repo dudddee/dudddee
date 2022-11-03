ba1ca1a23bf5a94fd95bafa709d48cb2b71b31e6
dudddee <116696782+dudddee@users.noreply.github.com>
2022-11-02 21:12:37 -0300

    Update ex20.py

--- projects/ex20.py
+++ projects/ex20.py
@@ -1,13 +1,21 @@
+#this exercise makes the program print each line at a time
 from sys import argv
 
 script, input_file = argv
 
+#here i define 3 functions
+
+#this one prints all the content in a open file
 def print_all(f):
     print(f.read())
+#think of a pointer, when i open the file later in the code, the pointer is in the first character
+#when i print the read method, the pointer goes through the file and prints it
 
+#this function rewinds the point to the position 0
 def rewind(f):
     f.seek(0)
 
+#this now prints each line
 def print_a_line(line_count, f):
     print(line_count, f.readline())
 
